77a26f62fe06ca5504d53354076b68f35b9598fe
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2021-11-22 17:21:45 +0100

    Add tests for the Roster and Event Log components.

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "../App";
+import Roster from "../components/Roster";
+import EventLog from "../components/EventLog";
+import { rounds } from "./__fixtures__/rounds";
+import { getPlayers } from "../DAL/Player";
 
-test("renders learn react link", () => {
+test("renders the app page", () => {
 	render(<App />);
-	const linkElement = screen.getByText(/React Royale/i);
-	expect(linkElement).toBeInTheDocument();
+	const title = screen.getByText(/React Royale/i);
+	expect(title).toBeInTheDocument();
+});
+
+describe("<Roster >", () => {
+	const players = getPlayers();
+
+	test("should render the title", () => {
+		render(<Roster players={players} />);
+		const title = screen.getByText(/Roster/i);
+		expect(title).toBeInTheDocument();
+	});
+});
+
+describe("<EventLog >", () => {
+	test("should render the title", () => {
+		render(<EventLog rounds={rounds} />);
+		const title = screen.getByText(/Event Log/i);
+		expect(title).toBeInTheDocument();
+	});
 });
---
+++ src/tests/__fixtures__/rounds.ts
@@ -0,0 +1,11 @@
+export const rounds = [
+	{
+		id: 1,
+		name: "Day 1",
+		events: [
+			{ id: 1, desc: "This is an event!" },
+			{ id: 2, desc: "This is an event!" },
+			{ id: 3, desc: "This is an event!" },
+		],
+	},
+];
