4f665574f5a7332ed6479ae11aa36f12320b1c18
Valls Martinez, Julio <julio.valls.martinez@oracle.com>
2021-11-22 09:23:12 +0100

    Fix player json loading.

--- src/DAL/Player.ts
+++ src/DAL/Player.ts
@@ -1,37 +1,38 @@
 import { CSSProperties } from "react";
 
 export interface PlayerState {
-    name: string;
-    bonus: number;
-    style: CSSProperties;
+	name: string;
+	bonus: number;
+	style: CSSProperties;
 }
 
 export const STATE_HEALTHY: PlayerState = {
-    name: "Healthy",
-    bonus: 0,
-    style: {color: "green"},
-}
+	name: "Healthy",
+	bonus: 0,
+	style: { color: "green" },
+};
 
 export interface Player {
-    key: string;
-    name: string;
-    weapon: string;
-    state: PlayerState;
+	key: string;
+	name: string;
+	weapon: string;
+	state: PlayerState;
 }
 
 function initializePlayers(players: Player[]): Player[] {
-    return players.map(player => ({
-        ...player,
-        weapon: "None",
-        state: STATE_HEALTHY,
-    }));
+	return players.map((player) => ({
+		...player,
+		weapon: "None",
+		state: STATE_HEALTHY,
+	}));
 }
 
-function loadPlayersJSON(playerJSON: string): Player[] {
-    return require(playerJSON);
+function loadPlayersJSON(): Player[] {
+    // Theres an issue if I pass the path as parameter.
+	return require("../assets/data/roster.json");
 }
 
 export function getPlayers() {
-    const players = loadPlayersJSON("../assets/data/roster.json")
-    return initializePlayers(players);
+	const players = loadPlayersJSON();
+	return initializePlayers(players);
 }
